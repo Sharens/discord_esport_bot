53525ae7dfca96b96fc06f3b4a1e609d9e37d5b5
Sharens <xarens19@gmail.com>
2024-11-25 22:10:34 +0100

    Aktualizacje skryptu bota

--- bot/client.py
+++ bot/client.py
@@ -1,8 +1,11 @@
 import discord
 from libs.config import Config
 
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
 
-bot = discord.Bot()
+bot = discord.Bot(intents=intents)
 
 
 @bot.event
@@ -10,7 +13,7 @@ async def on_ready():
     print(f"Zalogowano bota: {bot.user}")
 
 
-@bot.slash_command(guild_ids=Config.TEST_SERVER_ID)
+@bot.slash_command(name="hello", description="Say hello to bot")
 async def hello(ctx):
     await ctx.respond("Hello")
 
