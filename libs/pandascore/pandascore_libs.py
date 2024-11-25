c6692f9b2bf10d68cc621ecac9d06e6213115ca3
Sharens <xarens19@gmail.com>
2024-11-25 20:52:48 +0100

    Przywrócenie pierwotnego kodu bez rasy, pojedyncze zmiany do libsów pandascore

--- libs/pandascore/pandascore_libs.py
+++ libs/pandascore/pandascore_libs.py
@@ -1,6 +1,7 @@
 import requests
 from libs.config import Config
 import json
+import datetime
 
 pandascore_data_path = "libs/pandascore/pandascore_data.json"
 
@@ -18,13 +19,57 @@ def get_tournment_data():
 
     with open(pandascore_data_path, 'w') as output_file:
         output_file.write(tournaments)
+    print(f"Zaktualizowane dane zapisano w: {pandascore_data_path}")
 
 
-def structurize_data():
+def get_all_match_dict():
+    match_dict = dict()
+    get_tournment_data()
+
     with open(pandascore_data_path, 'r') as json_file:
         data = json.load(json_file)
-
     for ttr in data:
-        print(ttr['name'])
         for match in ttr['matches']:
-            print(match["name"], "#### Starts at", match["begin_at"])
+            match_id, match_name = match['id'], match["name"]
+            match_status = match["status"]
+            if match["begin_at"] is not None:
+                match_begin_at = datetime.datetime.strptime(
+                    match["begin_at"], "%Y-%m-%dT%H:%M:%SZ"
+                )
+            match_dict.update({
+                match_id: [
+                    match_name,
+                    match_status,
+                    match_begin_at
+                ]
+            })
+
+    return match_dict
+
+
+def get_active_match_dict():
+    active_match_dict = dict()
+    get_tournment_data()
+
+    with open(pandascore_data_path, 'r') as json_file:
+        data = json.load(json_file)
+    for ttr in data:
+        for match in ttr['matches']:
+            if match["status"] == "not_started":
+                match_status = match["status"]
+                match_id, match_name = match['id'], match["name"]
+
+                if match["begin_at"] is not None:
+                    match_begin_at = datetime.datetime.strptime(
+                        match["begin_at"], "%Y-%m-%dT%H:%M:%SZ"
+                    )
+
+                active_match_dict.update({
+                    match_id: [
+                        match_name,
+                        match_status,
+                        match_begin_at
+                    ]
+                })
+
+    return active_match_dict
