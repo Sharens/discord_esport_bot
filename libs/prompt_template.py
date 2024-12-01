a273679da1644b749614ff4a68373194033c6b42
Sharens <xarens19@gmail.com>
2024-12-01 09:53:49 +0100

    Dodanie prompta na wyświetlanie aktywnych rozgrywek; wygenerowanie requirments.txt

--- libs/prompt_template.py
+++ libs/prompt_template.py
@@ -1,3 +1,11 @@
-current_match = """
-Poniżej podaje dzisiejsze mecze z csa
+active_games_template = """
+Na podstawie przesłanego słownika: {games_dict}
+
+Zwróć zestawienie 10 najbliższych meczy ze statusem ('not_started'), wraz z planowanymi datami i godzinami rozgrywek z gry Counter Strike 2.
+Odpowiedź ma być dostosowana do czytania przez człowieka, nie wrzucaj raw data ze słownika.
+
+Schemat odpowiedzi:
+
+Aktualne rozgrywki e-sportowe z Counter Strike 2:
+- (<id rozgrywki>) <nazwa rozgrywki> | <data meczu> <godzina meczu>
 """
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import ollama
-response = ollama.chat(model='llama3.1', messages=[
-  {
-    'role': 'user',
-    'content': 'wygeneruj mi kod w pythonie który narysuje trójkąt który zostanie wyprintowany w formie tekstu',
-  },
-])
-print(response['message']['content'])
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from libs.pandascore.pandascore_libs import get_active_match_dict
+from libs.prompt_template import active_games_template
+
+active_games = get_active_match_dict()
+model = OllamaLLM(model="llama3.1")
+
+active_games_prompt = ChatPromptTemplate.from_template(
+    active_games_template
+)
+
+chain = active_games_prompt | model
+
+response = chain.invoke({"games_dict": active_games})
+print(response)
